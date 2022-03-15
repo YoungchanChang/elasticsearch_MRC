9c1430911a5542c38583d1d8c36dc08487d09e4b
youngchan <san0558@naver.com>
2022-03-15 17:21:25 +0900

    Update wikipedia_data.py
    - 위키피디아 데이터 한 라인씩 삽입하도록 변경

--- controller/wikipedia_data.py
+++ controller/wikipedia_data.py
@@ -28,10 +28,11 @@ def get_wiki_data(keyword: str) -> Generator:
             for page_test_item in page_sec_item.text.split("\n"):
                 if page_test_item == "" or page_test_item == "\t\t":
                     continue
-                yield WikiItem(title=page_py.title,
-                               first_header=page_section.title,
-                               second_header=page_sec_item.title,
-                               content=page_test_item)
+                for page_item_one_sentence in split_sentences(page_test_item):
+                    yield WikiItem(title=page_py.title,
+                                   first_header=page_section.title,
+                                   second_header=page_sec_item.title,
+                                   content=page_item_one_sentence)
 
 
 if __name__ == "__main__":
