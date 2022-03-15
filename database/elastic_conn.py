dc3cea9e434437cbb768a749efb768bbf112582f
youngchan <san0558@naver.com>
2022-03-15 16:53:16 +0900

    Update elastic_conn.py
    - 유저 비밀번호 업데이트

--- database/elastic_conn.py
+++ database/elastic_conn.py
@@ -2,6 +2,6 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(
     ['https://localhost:9200'],
-    basic_auth=("elastic", "Gv0QarYgz5da0Frv_cH*"),
+    basic_auth=("elastic", "6bXz4stf_*78WWZgiDPH"),
     ca_certs="/Users/youngchan/Desktop/MRC/database/http_ca.crt",
 )
