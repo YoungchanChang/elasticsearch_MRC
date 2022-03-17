e201e004d2804b73c3441ff20d99c4fbf2669bb9
youngchan <san0558@naver.com>
2022-03-17 14:15:47 +0900

    Add gen_mrc_data in mrc_control.py
    - mrc_control 템플릿형으로 변경

--- app/controller/mrc_control.py
+++ app/controller/mrc_control.py
@@ -5,56 +5,77 @@ import requests
 from requests.auth import HTTPBasicAuth
 from pororo import Pororo
 
+
+from app.application.wikipedia_data import get_wiki_data
+from app.domain.elastic_domain import get_knn_template
+from main import elastic_index
+
 se = Pororo(task="sentence_embedding", lang="ko")
 json_header = {'Content-Type':'application/json'}
 mrc = Pororo(task="mrc", lang="ko")
 
 MRC_TOKEN_SENTENCE_LIMIT = 5
 
-def get_question_template(question: str) -> dict:
-    question_val = se(question)
-    pororo_list = np.array(question_val).tolist()
 
-    field = {
-      "knn": {
-        "field": "content-vector",
-        "query_vector": pororo_list,
-        "k": 10,
-        "num_candidates": 12
-      },
-      "fields": [
-        "content-vector",
-        "content"
-      ]
-    }
-    return field
+def gen_mrc_data(keyword):
 
+    """
+    mrc에 데이터 삽입하는 함수
+    :param keyword:
+    :return:
+    """
 
-def get_elasticsearch_knn(question: str):
+    for page_item in get_wiki_data(keyword):
 
-    resp = requests.get("https://localhost:9200/my-approx-knn-index-test/_knn_search",
-                         json=get_question_template(question),
-                        headers=json_header,
-                         auth=HTTPBasicAuth("elastic", "6bXz4stf_*78WWZgiDPH"),
-                         verify=False)
-
-    resp = (json.loads(resp.text))
-
-    content = []
-    for idx, hit in enumerate(resp['hits']['hits']):
-        content.append(hit["_source"]['content'])
-
-    return content
+        yield {
+            "_index": elastic_index,
+            "_source": {
+                    "content-vector": np.array(se(page_item.content)).tolist(),
+                    "title": page_item.title,
+                    "first_header": page_item.first_header,
+                    "second_header": page_item.second_header,
+                    "content": page_item.content
+                }
+            }
 
 
-def get_pororo_answer(question: str):
-    content = get_elasticsearch_knn(question=question)
-    mrc_answer = mrc(
-       question,
-       " ".join(content[:MRC_TOKEN_SENTENCE_LIMIT])
-    )
-    return mrc_answer
+class MRC:
+
+    def get_question_template(self, question: str) -> dict:
+        question_val = se(question)
+        pororo_list = np.array(question_val).tolist()
+
+        field = get_knn_template(pororo_list)
+
+        return field
+
+
+    def get_elasticsearch_knn(self, question: str):
+
+        resp = requests.get("https://localhost:9200/my-approx-knn-index-test/_knn_search",
+                             json=self.get_question_template(question),
+                            headers=json_header,
+                             auth=HTTPBasicAuth("elastic", "6bXz4stf_*78WWZgiDPH"),
+                             verify=False)
+
+        resp = (json.loads(resp.text))
+
+        content = []
+        for idx, hit in enumerate(resp['hits']['hits']):
+            content.append(hit["_source"]['content'])
+
+        return content
+
+
+    def get_pororo_answer(self, question: str):
+        content = self.get_elasticsearch_knn(question=question)
+        mrc_answer = mrc(
+           question,
+           " ".join(content[:MRC_TOKEN_SENTENCE_LIMIT])
+        )
+        return mrc_answer
 
 
 if __name__ == "__main__":
-    print(get_pororo_answer("조선시대 최고 학부는 어디야"))
+    mrc = MRC()
+    print(mrc.get_pororo_answer("조선시대 최고 학부는 어디야"))
