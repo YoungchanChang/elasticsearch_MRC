1c814bb71bf5d7dae3c0823104bd897c62b08656
youngchan <san0558@naver.com>
2022-03-17 14:16:18 +0900

    Add get_knn_template in elastic_domain.py
    - mrc 질의 시 템플릿형으로 변경

--- app/domain/elastic_domain.py
+++ app/domain/elastic_domain.py
@@ -1,19 +1,26 @@
+from typing import List
+
 K_NEAR_NUM = 10
 NUM_CANDIDATE = 12
 ONLY_ONE_TITLE = 1
 
-elastic_search_field = {
-      "knn": {
-        "field": "content-vector",
-        "query_vector": [],
-        "k": K_NEAR_NUM,
-        "num_candidates": NUM_CANDIDATE
-      },
-      "fields": [
-        "content-vector",
-        "content"
-      ]
-    }
+
+def get_knn_template(k: List):
+
+    elastic_search_field = {
+          "knn": {
+            "field": "content-vector",
+            "query_vector": k,
+            "k": K_NEAR_NUM,
+            "num_candidates": NUM_CANDIDATE
+          },
+          "fields": [
+            "content-vector",
+            "content"
+          ]
+        }
+
+    return elastic_search_field
 
 
 def get_es_title_template(keyword: str):
