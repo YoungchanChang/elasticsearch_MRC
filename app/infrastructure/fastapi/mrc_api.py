eaa5205411f9f81f1387b09d614e1f1536ddd306
youngchan <san0558@naver.com>
2022-03-20 19:11:04 +0900

    Add insert_data in mrc_api.py
    - 데이터 삽입 api 추가

--- app/infrastructure/fastapi/mrc_api.py
+++ app/infrastructure/fastapi/mrc_api.py
@@ -7,7 +7,7 @@ from fastapi.encoders import jsonable_encoder
 from app.controller.mrc_control import MRC
 from app.domain.custome_error import WikiDataException
 from app.domain.domain import WikiQuestionItem, WikiQuestionItemDTO
-from app.infrastructure.database.elastic_repository import WikiSearch
+from app.infrastructure.database.elastic_repository import WikiSearch, ElasticContent
 
 formatter = "%(asctime)s.%(msecs)03d\t%(levelname)s\t[%(name)s]\t%(message)s"
 logging.basicConfig(level=logging.DEBUG, format=formatter, datefmt='%m/%d/%Y %I:%M:%S')
@@ -22,18 +22,38 @@ router = APIRouter(
 
 
 @router.post("/wiki_question")
-async def create_index(wiki_question_item: WikiQuestionItem):
+async def find_data(wiki_question_item: WikiQuestionItem):
     mrc = MRC()
-    wiki_search = WikiSearch()
 
     try:
-        wiki_answer = wiki_search.find_one(WikiQuestionItemDTO(title=wiki_question_item.title, question=wiki_question_item.question))
 
-        if not wiki_answer:
-            raise Exception
+        mrc_answer, best_proper_content = mrc.filter_mrc_content(wiki_question_item.question)
 
-        result = mrc.get_pororo_answer(wiki_question_item.question)
-        return_json = {"result": result[MRC_ANSWER]}
+        return_json = {"mrc_answer": mrc_answer,
+                       "best_proper_content": best_proper_content,}
+
+        return jsonable_encoder(return_json)
+
+    except WikiDataException as wde:
+        return_json = {"result": "위키피디아에 없는 주제입니다."}
+        logging.critical({'status': 'fail', "message": wde})
+        return jsonable_encoder(return_json)
+
+    except Exception as e:
+        error_msg = traceback.format_exc()
+        logging.critical({'status': 'fail', "message": error_msg})
+        return_json = {"result": "서버에 문제가 생겼습니다. 관리자에게 문의하세요."}
+        return jsonable_encoder(return_json)
+
+
+@router.post("/wiki_insert")
+async def insert_data(wiki_question_item: WikiQuestionItem):
+
+    try:
+
+        result = ElasticContent().create(WikiQuestionItemDTO(title=wiki_question_item.question, question=None))
+        return_json = {"result": result}
+
         return jsonable_encoder(return_json)
 
     except WikiDataException as wde:
