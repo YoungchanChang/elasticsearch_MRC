7330551fcf2bf84b0d151b44220f14c6d9f92b36
youngchan <san0558@naver.com>
2022-03-20 16:59:34 +0900

    Add elastic_vector_index.py
    - 엘라스틱서치 벡터 검색을 위한 인덱스 값 추가

---
+++ scripts/elastic_vector_index.py
@@ -0,0 +1,93 @@
+from app.infrastructure.database.elastic_conn import *
+from app.config.settings import *
+
+
+def set_wiki_index(elastic_index: str):
+
+    result = es.indices.delete(index=elastic_index, ignore=[400, 404])
+    print(result)
+
+    body = {
+        "settings": {
+            "index": {
+                "analysis": {
+                    "analyzer": {
+                        "nori_analyzer": {
+                            "type": "custom",
+                            "tokenizer": "nori_tokenizer",
+                            "filter": [
+                                "part_of_speech_stop_sp",
+                            ]
+                        },
+                        "nori_elastic_analyzer": {
+                            "type": "custom",
+                            "tokenizer": "nori_tokenizer",
+                            "filter": [
+                                "part_of_speech_stop_sp",
+                            ]
+                        }
+                    },
+                    # 동의어 추가 및 제거할 단어
+                    "filter": {
+                        "part_of_speech_stop_sp": {
+                            "type": "nori_part_of_speech",
+                            "stoptags": [
+                                "E",
+                                "IC",
+                                "J",
+                                "MAG", "MAJ", "MM",
+                                "SP", "SSC", "SSO", "SC", "SE",
+                                "XPN", "XSA", "XSN", "XSV",
+                                "UNA", "NA", "VSV"
+                            ]
+                        }
+                    }
+                }
+            }
+        },
+        "mappings": {
+            "properties": {
+                "title-vector": {
+                    "type": "dense_vector",
+                    "dims": 768,
+                    "index": True,
+                    "similarity": "cosine"
+                },
+                "content-vector": {
+                    "type": "dense_vector",
+                    "dims": 768,
+                    "index": True,
+                    "similarity": "cosine"
+                },
+                "title": {
+                    "type": "keyword",
+                },
+                "first_header": {
+                    "type": "keyword"
+                },
+                "second_header": {
+                    "type": "keyword"
+
+                },
+                "content": {
+                    "type": "text",
+                    "analyzer": "nori_analyzer",
+                    "fielddata": True
+                }
+            }
+        }
+    }
+
+    ans = es.indices.create(index=elastic_index, body=body, ignore=400, )
+    print(ans)
+
+
+def exists_index():
+    es_alias = es.indices.get_alias().keys()
+    if "wiki-vector-index" not in es_alias:
+        set_wiki_index()
+
+if __name__ == '__main__':
+
+    exists_index(elastic_vector_index)
+
