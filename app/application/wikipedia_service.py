2316ce1cb8c104e5b8598482be9871a95ecab262
youngchan <san0558@naver.com>
2022-03-20 17:05:29 +0900

    Add wikipedia_service.py
    - 위키피디아 서비스 객체 추가

---
+++ app/application/wikipedia_service.py
@@ -0,0 +1,15 @@
+from app.application.repo_interface import AbstractFinder
+from app.domain.domain import WikiQuestionItemDTO
+
+
+class WikipediaService:
+
+    def __init__(self, repository: AbstractFinder):
+        self.repository = repository
+
+    def get_wiki_data(self, title: str):
+        wiki_question = WikiQuestionItemDTO(title=title,question=None)
+        wiki_data = self.repository.find_one(model=wiki_question)
+        return wiki_data
+
+
