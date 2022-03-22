17c0c3b2172f9a3fb0c23951a188a385aac66ff0
youngchan <san0558@naver.com>
2022-03-22 22:20:34 +0900

    Update elastic_conn.py
    - 엘라스틱서치 재가동 관련 elastic pwd 변경

--- app/infrastructure/database/elastic_conn.py
+++ app/infrastructure/database/elastic_conn.py
@@ -2,6 +2,6 @@ from elasticsearch import Elasticsearch
 
 es = Elasticsearch(
     ['https://localhost:9200'],
-    basic_auth=("elastic", "6bXz4stf_*78WWZgiDPH"),
+    basic_auth=("elastic", "O*hTYJDverJ48HLGTQr7"),
     ca_certs="/Users/youngchan/Desktop/MRC/app/infrastructure/database/http_ca.crt",
 )
