85c9a60a8366aa80b8082e9071cf65133a2761ae
youngchan <san0558@naver.com>
2022-03-26 22:29:57 +0900

    Add entity.py
    - 질의문 처리 데이터 : QueryDomain
    - 엘라스틱서치를 위한 질의문 처리 데이터 : ElasticSearchDomain
    - mrc에 질의하기 위한 질의문 처리 데이터 : MrcDomain
    - 엘라스틱서치에 insert하기 위한 질의문 처리 데이터 : ElasticIndexDomain

---
+++ app/domain/entity.py
@@ -0,0 +1,47 @@
+from dataclasses import dataclass
+from typing import List
+
+
+@dataclass
+class Domain:
+    ...
+
+
+@dataclass
+class QueryDomain(Domain):
+    """
+    기본 질의문
+    """
+    query: str
+
+
+@dataclass
+class ElasticSearchDomain(QueryDomain):
+    """
+    엘라스틱서치 검색을 위한 데이터 클래스
+    """
+    query_vector: List
+    noun_tokens: List
+    verb_tokens: List
+
+
+@dataclass
+class MrcDomain(QueryDomain):
+    """
+    MRC에 질의하기 위한 데이터 클래스
+    """
+    search_string: str
+
+
+@dataclass
+class ElasticIndexDomain(Domain):
+    """
+    엘라스틱서치에 insert하기 위한 클래스
+    """
+    content_vector: List
+    title: str
+    first_header: str
+    second_header: str
+    content: str
+    content_noun_tokens: List
+    content_verb_tokens: List
