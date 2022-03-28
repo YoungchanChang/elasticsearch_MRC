e71d02abbb02387cc771015736d6e4290e0007ce
youngchan <san0558@naver.com>
2022-03-28 22:20:09 +0900

    Update entity.py
    - 필요 엔티티 클래스 제외 나머지 입출력 컨트롤러로 이동

--- app/domain/entity.py
+++ app/domain/entity.py
@@ -8,30 +8,14 @@ class Domain:
 
 
 @dataclass
-class WikiTitle(Domain):
-    title: str
-
-
-@dataclass
-class WikiItem(WikiTitle):
-    first_header: str
-    second_header: str
-    content: str
-
-
-@dataclass
 class QueryDomain(Domain):
-    """
-    기본 질의문
-    """
+    """사용자 질의문"""
     query: str
 
 
 @dataclass
-class ElasticSearchDomain(QueryDomain):
-    """
-    엘라스틱서치 검색을 위한 데이터 클래스
-    """
+class KeywordVectorDomain(QueryDomain):
+    """질의의 키워드와 벡터값 정보를 포함하는 데이터 클래스"""
     query_vector: List
     noun_tokens: List
     verb_tokens: List
@@ -39,22 +23,6 @@ class ElasticSearchDomain(QueryDomain):
 
 
 @dataclass
-class ElasticIndexDomain(Domain):
-    """
-    엘라스틱서치에 insert하기 위한 클래스
-    """
-    content_vector: List
-    title: str
-    first_header: str
-    second_header: str
-    content: str
-    content_noun_tokens: List
-    content_verb_tokens: List
-
-
-@dataclass
 class MrcDomain(QueryDomain):
-    """
-    MRC에 질의하기 위한 데이터 클래스
-    """
+    """MRC에 질의하기 위한 데이터 클래스"""
     search_string: str
