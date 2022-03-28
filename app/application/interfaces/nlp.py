0f192deb5da610a7af3dcc8ecc06945c3cdf3c0a
youngchan <san0558@naver.com>
2022-03-28 20:33:14 +0900

    Update nlp.py
    - NLP 유즈케이스에 입력값 변경

--- app/application/interfaces/nlp.py
+++ app/application/interfaces/nlp.py
@@ -1,7 +1,7 @@
 import abc
 from typing import List
 
-from app.domain.entity import Domain
+from app.domain.entity import Domain, QueryDomain
 
 
 class AbstractNLP(abc.ABC):
@@ -10,13 +10,13 @@ class AbstractNLP(abc.ABC):
         ...
 
     @abc.abstractmethod
-    def get_embeddings(self, domain: Domain) -> List:
+    def get_embeddings(self, domain: QueryDomain) -> List:
         ...
 
     @abc.abstractmethod
-    def get_nouns_verbs(self, domain: Domain) -> (List, List):
+    def get_nouns_verbs(self, domain: QueryDomain) -> (List, List):
         ...
 
     @abc.abstractmethod
-    def get_pos(self, domain: Domain) -> List:
+    def get_pos(self, domain: QueryDomain) -> List:
         ...
